54c58dcd23a58170477d6e4e3f6ce79a5695ab6e
scaplan <github@spencercaplan.org>
2017-05-18 13:47:15 -0400

    Child (output) noun and verb type tabulation

--- extract-single-utterance.py
+++ extract-single-utterance.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys, math, os, subprocess, glob
+import sys, math, os, subprocess, glob, nltk
+from nltk import word_tokenize
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import unicodedata
 from unicodedata import normalize
 
 punctuationSet = ['.', '?', '!', ':', '(.)', '+...', '+"/.', '+/.']
-inputSet = ['*mot:', '*gra:', '*fat:', '*ann:', '*ant:', '*nan:', '*wom:']
+inputSet = ['*mot:', '*gra:', '*fat:', '*ann:', '*ant:', '*nan:', '*wom:', '*car:']
 childSet = ['*chi:', '*eli:', '*gre:', '*mar:']
 morphCue = '%mor:'
 inputNounTypeDict = {}
@@ -23,7 +24,7 @@ missingSpeechLines = 0
 
 def readChaFiles():
 	for dataFileName in glob.glob("*.cha"):
-		print (os.getcwd() + '/' + dataFileName)
+	#	print (os.getcwd() + '/' + dataFileName)
 		with open(dataFileName, 'r') as currFile:
 			# store tuples
 			speechGroup = []
@@ -66,6 +67,9 @@ def evalSpeechGroup(speechGroup):
 			totalMotherLines += 1
 			cumulativeMotherLinesLength += (len(currLineTokensNoPunc) - 1)
 			evalInputData(currLineTokensNoPunc, tagTokensNoPunc)
+			
+		#	print speechLine
+			
 		elif (currLineTokensNoPunc[0] in childSet):
 			totalChildLines += 1
 			cumulativeChildLinesLength += (len(currLineTokensNoPunc) - 1)
@@ -75,24 +79,30 @@ def evalSpeechGroup(speechGroup):
 
 def evalInputData(inputString, inputTags):
 	global totalSingleUtteranceMotherLines, inputNounTypeDict, inputVerbTypeDict, singleWordNouns, singleWordVerbs
+
+#	toTag = inputString[1:]
+#	taggedInput = nltk.pos_tag(toTag)
+
 	if (len(inputString) == 2):
 		totalSingleUtteranceMotherLines += 1
 		if (len(inputTags) > 0):
 			wordTag = inputTags[1]
 			wordTagInfo = wordTag.split("|")
+			wordMarkupForm = wordTagInfo[1]
+			#wordMarkupForm = inputString[1]
 			if (wordTagInfo[0] == 'n'):
-				if (inputString[1] in inputNounTypeDict):
-					newCount = 1 + inputNounTypeDict.get(inputString[1])
-					inputNounTypeDict[inputString[1]] = newCount
+				if (wordMarkupForm in inputNounTypeDict):
+					newCount = 1 + inputNounTypeDict.get(wordMarkupForm)
+					inputNounTypeDict[wordMarkupForm] = newCount
 				else:
-					inputNounTypeDict[inputString[1]] = 1
+					inputNounTypeDict[wordMarkupForm] = 1
 				singleWordNouns += 1
 			elif (wordTagInfo[0] == 'v'):
-				if (inputString[1] in inputVerbTypeDict):
-					newCount = 1 + inputVerbTypeDict.get(inputString[1])
-					inputVerbTypeDict[inputString[1]] = newCount
+				if (wordMarkupForm in inputVerbTypeDict):
+					newCount = 1 + inputVerbTypeDict.get(wordMarkupForm)
+					inputVerbTypeDict[wordMarkupForm] = newCount
 				else:
-					inputVerbTypeDict[inputString[1]] = 1
+					inputVerbTypeDict[wordMarkupForm] = 1
 				singleWordVerbs += 1
 		#	print " ".join(inputString) + " ::: " + wordTagInfo[0]
 
@@ -110,7 +120,22 @@ def evalOutputData(outputString, outputTags):
 		elif ('v|' in currTag):
 			outputVerbTokenCount += 1
 
-		# currTagInfo = currTag.split("|")
+		currTagInfo = currTag.split("|")
+		if len(currTagInfo) > 1:
+			currPos = currTagInfo[0]
+			currWord = currTagInfo[1]
+			if (currPos == 'v'):
+				if (currWord in outputVerbTypeDict):
+					newCount = 1 + outputVerbTypeDict.get(currWord)
+					outputVerbTypeDict[currWord] = newCount
+				else:
+					outputVerbTypeDict[currWord] = 1
+			elif (currPos == 'n'):
+				if (currWord in outputNounTypeDict):
+					newCount = 1 + outputNounTypeDict.get(currWord)
+					outputNounTypeDict[currWord] = newCount
+				else:
+					outputNounTypeDict[currWord] = 1
 		# if (currTagInfo[0] == 'n'):
 		# 	outputNounTokenCount += 1
 		# elif (currTagInfo[0] == 'v'):
@@ -128,6 +153,11 @@ def iterateSubDir(directoryName):
 		os.chdir(subDirPath)
 		readChaFiles()
 
+def safeDivide(numerator, denominator):
+	if denominator > 0:
+		return (numerator / (denominator * 1.0))
+	else:
+		return 0.0
 
 ##
 ## Main method block
@@ -152,8 +182,8 @@ if __name__=="__main__":
 	searchDirectory = os.getcwd() + '/' + directoryName
 	iterateSubDir(searchDirectory)
 
-	motherMLU = cumulativeMotherLinesLength / (totalMotherLines * 1.0)
-	childMLU = cumulativeChildLinesLength / (totalChildLines * 1.0)
+	motherMLU = safeDivide(cumulativeMotherLinesLength, totalMotherLines)
+	childMLU = safeDivide(cumulativeChildLinesLength, totalChildLines)
 
 	print 'totalDataLength: ' + str(totalDataLength)
 	print 'totalMotherLines: ' + str(totalMotherLines)
@@ -168,6 +198,8 @@ if __name__=="__main__":
 	print 'totalChildLines: ' + str(totalChildLines)
 	print 'child MLU: ' + str(childMLU)
 	print 'childNounTokens: ' + str(outputNounTokenCount)
+	print 'childNounTypes: ' + str(len(outputNounTypeDict))
 	print 'childVerbTokens: ' + str(outputVerbTokenCount)
+	print 'childverbTypes: ' + str(len(outputVerbTypeDict))
 
 	print 'missingSpeechLines: ' + str(missingSpeechLines)
